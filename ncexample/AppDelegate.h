e8ab48ff4bc1208424501a1dd9c7d67e7c149dad
Murat Yılmaz <muratylmz89@gmail.com>
2018-12-19 18:37:01 +0300

    inital commit
    initial commit for github

--- ncexample/AppDelegate.h
+++ ncexample/AppDelegate.h
@@ -14,6 +14,7 @@
 
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) AppRouter *router;
+
 @property (readonly, strong) NSPersistentContainer *persistentContainer;
 
 - (void)saveContext;
